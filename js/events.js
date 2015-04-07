a4c93bb7b5c4ed7b351f4396b28a01eceb827914
WillHK <will.haeck@gmail.com>
2015-04-07 14:42:52 -0700

    basic html for adding new stores

---
+++ js/events.js
@@ -0,0 +1,7 @@
+(function () {
+  var button = document.getElementById('newStoreButton');
+  button.addEventListener("submit", function() {
+    var newStore = new TopPot ();
+
+  });
+}) ();
--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 (function () {
-  var TopPot = function (min, max, average, name) {
+  window.stores = [];
+  var TopPot = function (min, max, average, name, hoursOpen) {
     this.min = min;
     this.max = max;
     this.average = average;
     this.hourlyTotals = [];
     this.name = name;
+    this.hoursOpen = hoursOpen || 11;
+    this.openTime = '7:00 AM';
+    this.closeTime = '6:00 PM';
   };
+
+  TopPot.prototype.randomCustomerGenerator = function () {
+
+  };
+
   TopPot.prototype.donutsPerHour = function () {
     var numberCustomers = Math.floor(Math.random() * this.max - this.min + 1) + this.min;
     return Math.round(numberCustomers * this.average);
@@ -13,7 +22,7 @@
 
   TopPot.prototype.donutsPerDay = function () {
     var perDay = 0;
-    for (var i = 0; i <= 11; i++) {
+    for (var i = 0; i < this.hoursOpen; i++) {
       var hourlyTotal = this.donutsPerHour();
       this.hourlyTotals.push(hourlyTotal);
       perDay += hourlyTotal;
@@ -24,7 +33,7 @@
   TopPot.prototype.render = function () {
     var dailyTotal = this.donutsPerDay();
     var elTr = document.getElementById(this.name);
-    for (var i = 0; i <= this.hourlyTotals.length; i++) {
+    for (var i = 0; i <= this.hoursOpen; i++) {
       var el = document.createElement('td');
       el.textContent = this.hourlyTotals[i];
       elTr.appendChild(el);
@@ -32,6 +41,9 @@
     el.textContent = dailyTotal;
     elTr.appendChild(el);
   };
+  window.stores.push(new TopPot(8, 43, 4.50, "store1"));
+  window.stores.push(new TopPot(4, 37, 2.00, "store2"));
+  console.log(window.stores);
   var downtown = new TopPot(8, 43, 4.50, "store1");
   var capitolHill = new TopPot(4, 37, 2.00, "store2");
   var slu = new TopPot(9, 23, 6.33, "store3");
